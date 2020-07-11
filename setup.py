74f8183b52d5fda438dc2290c576d31037bb5103
Michael Flaxman <mflaxman@gmail.com>
2020-07-11 11:26:33 -0500

    update example

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/secondguard/secondguard-python/',
-    version='2.2.1',
+    version='2.2.2',
     install_requires=REQUIREMENTS,
     packages=find_packages(),
     classifiers=[
