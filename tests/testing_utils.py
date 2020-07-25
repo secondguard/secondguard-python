c4fbf81723bcd0056102313b38bac28922c73dc2
Michael Flaxman <mflaxman@gmail.com>
2020-07-25 16:56:44 -0500

    fix pubkey fetch

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -17,5 +17,5 @@ with open(os.path.join(key_dir, "insecurepubkey.crt"), "r") as f:
     PUBKEY_STR = f.read()
 
 
-def _fetch_testing_pubkey(url=BASE_URL + "static/pubkey.crt"):
+def _fetch_testing_pubkey(url=BASE_URL + "static/kek.pub"):
     return requests.get(url).content.decode("utf-8")
