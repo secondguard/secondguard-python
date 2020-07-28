747cb3fe4ce967769c1061be47e479929564dabd
Michael Flaxman <mflaxman@gmail.com>
2020-07-27 22:23:10 -0500

    remove dist folder after updating pypi (to prevent attempting re-upload later)

--- update_pypi.sh
+++ update_pypi.sh
@@ -1,1 +1,1 @@
-date && pytest -v && git push && python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/* && date
+date && pytest -v && git push && python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/* && date && rm -rf dist/
